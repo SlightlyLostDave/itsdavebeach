e1b3a615e39f3e6e7c6a5dc695b5f1184e9e608f
Dave Beach <dave@slightlylost.com>
2019-01-01 21:25:54 -0500

    Removed junk from wp_head()

--- wp-content/themes/itsdavebeach/functions.php
+++ wp-content/themes/itsdavebeach/functions.php
@@ -79,6 +79,13 @@ if ( ! function_exists( 'itsdavebeach_setup' ) ) :
 			'flex-width'  => true,
 			'flex-height' => true,
 		) );
+
+		remove_action('wp_head', 'rsd_link');
+		remove_action('wp_head', 'wlwmanifest_link');
+		remove_action('wp_head', 'wp_generator');
+		remove_action('wp_head', 'start_post_rel_link');
+		remove_action('wp_head', 'index_rel_link');
+		remove_action('wp_head', 'adjacent_posts_rel_link');
 	}
 endif;
 add_action( 'after_setup_theme', 'itsdavebeach_setup' );
--- wp-content/themes/itsdavebeach/header.php
+++ wp-content/themes/itsdavebeach/header.php
@@ -8,21 +8,36 @@
  *
  * @package It\'s_Dave_Beach
  */
-
 ?>
-<!doctype html>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
 </head>
-
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'itsdavebeach' ); ?></a>
+
+	<button class="hamburger" type="button" aria-label="Menu" aria-controls="navigation">
+		<span class="hamburger-box">
+			<span class="hamburger-inner"></span>
+		</span>
+	</button>
+	<nav id="site-navigation" class="main-navigation">
+		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'itsdavebeach' ); ?></button>
+		<?php
+		wp_nav_menu( array(
+			'theme_location' => 'menu-1',
+			'menu_id'        => 'primary-menu',
+		) );
+		?>
+	</nav><!-- #site-navigation -->
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
@@ -43,16 +58,6 @@
 				<p class="site-description"><?php echo $itsdavebeach_description; /* WPCS: xss ok. */ ?></p>
 			<?php endif; ?>
 		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'itsdavebeach' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
