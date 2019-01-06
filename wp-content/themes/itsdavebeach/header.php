a769a315492c38e74015c7cc08f61f4a5d346fc1
Dave Beach <dave@slightlylost.com>
2019-01-06 01:42:51 -0500

    Started blank theme

--- wp-content/themes/itsdavebeach/header.php
+++ wp-content/themes/itsdavebeach/header.php
@@ -6,58 +6,24 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package It\'s_Dave_Beach
+ * @package It's_Dave_Beach
  */
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-
-	<?php wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'itsdavebeach' ); ?></a>
+    <a href="#content">Skip to content</a>
 
-	<button class="hamburger" type="button" aria-label="Menu" aria-controls="navigation">
+    <button class="hamburger" type="button" aria-label="Menu" aria-controls="navigation">
 		<span class="hamburger-box">
 			<span class="hamburger-inner"></span>
 		</span>
 	</button>
-	<nav id="site-navigation" class="main-navigation">
-		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'itsdavebeach' ); ?></button>
-		<?php
-		wp_nav_menu( array(
-			'theme_location' => 'menu-1',
-			'menu_id'        => 'primary-menu',
-		) );
-		?>
-	</nav><!-- #site-navigation -->
-
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$itsdavebeach_description = get_bloginfo( 'description', 'display' );
-			if ( $itsdavebeach_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $itsdavebeach_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
